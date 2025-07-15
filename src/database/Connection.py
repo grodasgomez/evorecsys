dea7700c11dc3734fd234c87a8e69ab94f663a92
Guillermo Rodas Gomez <grodasgomez@gmail.com>
2025-07-15 09:36:18 -0300

    chore: update Docker configuration and README for improved setup
    - Added PYTHONUNBUFFERED environment variable in docker-compose.yml for better logging.
    - Mounted the current directory to /app in the container and specified the command to run the application.
    - Updated Dockerfile to create a virtual environment and install dependencies within it.
    - Modified database connection settings in Connection.py to use environment variables for better configuration management.
    - Enhanced README with installation instructions for building and starting the server and database.

--- src/database/Connection.py
+++ src/database/Connection.py
@@ -1,12 +1,14 @@
+import os
 # This is an abstract class. It is used to create the three connection objects used in the full workflow of
 # EvoRecSys.
+
 class Connection:
 
     # These values should be changed according to your network configurations.
-    HOST = 'localhost'  
-    USER = 'root'
-    PASSWORD = 'rootpassword'
-    DATA_BASE = 'evo_rec_sys_v2'
+    HOST = os.environ.get('MYSQL_HOST')
+    USER = os.environ.get('MYSQL_USER')
+    PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    DATA_BASE = os.environ.get('MYSQL_DATABASE')
 
     # Constructor
     def __init__(self):
